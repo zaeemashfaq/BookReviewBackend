6de839bf8d3f691dd82ea1c8d3b0e260cc6aefdc
Zaeem Ashfaq <zaeemashfaq@gmail.com>
2024-04-09 04:20:28 +0530

    Added additional allowed origins to CorsRegistry to allow request from EC2 http and https ports

--- src/main/java/dev/zaeem/bookreviewapp/configuration/WebConfig.java
+++ src/main/java/dev/zaeem/bookreviewapp/configuration/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","http://localhost:3001","http://localhost:3002")
+                .allowedOrigins("http://localhost:80","http://localhost:443","http://localhost:3000","http://localhost:3001","http://localhost:3002")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
